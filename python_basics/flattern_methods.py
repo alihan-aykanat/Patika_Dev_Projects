cf06df0cb49a7a8c3534a28439d05a9e352240c2
Alihan Aykanat <aykanat502@gmail.com>
2022-05-08 22:56:25 +0300

    flatten yapıldı

--- python_basics/flattern_methods.py
+++ python_basics/flattern_methods.py
@@ -1,11 +1,11 @@
 input = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
 
-# def deco_list(func):
-#     def inner(*args, **kwargs):
-#         return list(func(*args, **kwargs))    
-#     return inner    
+def deco_list(func):
+    def inner(*args, **kwargs):
+        return list(func(*args, **kwargs))    
+    return inner    
             
-# @deco_list
+@deco_list
 def flatten(list):
     
     for i in list:
@@ -13,8 +13,6 @@ def flatten(list):
             yield from flatten(i)
         else:
             yield i
-            
-    return list(list(flatten(list)))
     
 
 print(flatten(input))
