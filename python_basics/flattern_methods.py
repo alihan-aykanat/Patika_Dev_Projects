c381735917fbde769f1b4fce23afc4ecf3cc665a
Alihan Aykanat <aykanat502@gmail.com>
2022-05-08 22:56:10 +0300

    flatten projesi yapıldı

---
+++ python_basics/flattern_methods.py
@@ -0,0 +1,22 @@
+input = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
+
+# def deco_list(func):
+#     def inner(*args, **kwargs):
+#         return list(func(*args, **kwargs))    
+#     return inner    
+            
+# @deco_list
+def flatten(list):
+    
+    for i in list:
+        if hasattr(i, "__iter__") and not isinstance(i, (str,bytes)):
+            yield from flatten(i)
+        else:
+            yield i
+            
+    return list(list(flatten(list)))
+    
+
+print(flatten(input))
+
+
